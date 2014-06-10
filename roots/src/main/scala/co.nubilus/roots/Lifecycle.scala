4f28512fcb900cddbca368aff7fabbc6b5bdbd10
Greg Zoller <greg.zoller@bottlerocketapps.com>
2014-06-10 17:39:48 -0500

    Tried multi-jvm testing

--- roots/src/main/scala/co.nubilus/roots/Lifecycle.scala
+++ roots/src/main/scala/co.nubilus/roots/Lifecycle.scala
@@ -5,8 +5,6 @@ package roots
 	Lifecycle event callbacks.  These are no-op by default.  Implementations can override to taste.
  */
 trait Lifecycle {
-	private[roots] def init( r:Roots )
-
 	def preStartup( r:Roots )  {}
 	def postStartup( r:Roots ) {}
 
